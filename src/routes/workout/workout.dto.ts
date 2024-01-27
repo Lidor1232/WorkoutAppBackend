8ca38fc39fe6bd52a6556370efed78b3acb5c455
lidordvir <lidor4001@gmail.com>
2024-01-27 11:04:32 +0200

    * WIP

--- src/routes/workout/workout.dto.ts
+++ src/routes/workout/workout.dto.ts
@@ -1,9 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 import { CreateExercise } from '../exercise/exercise.dto';
 
 export class CreateWorkout {
   @IsString()
   date: string;
 
+  @IsArray()
   exercises: CreateExercise[];
 }
