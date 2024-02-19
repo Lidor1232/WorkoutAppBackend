c787986578229881e36dc3a0f4114cd9085aeeed
lidordvir <lidor4001@gmail.com>
2024-02-19 16:39:11 +0200

    * Change mongoDB database connection to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { UserModule } from './routes/user/user.module';
 import { WorkoutModule } from './routes/workout/workout.module';
 import { ExerciseModule } from './routes/exercise/exercise.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import environmentConfig from './config/environment.config';
 
 @Module({
-  imports: [UserModule, WorkoutModule, ExerciseModule],
+  imports: [
+    UserModule,
+    WorkoutModule,
+    ExerciseModule,
+    MongooseModule.forRoot(environmentConfig.database),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
