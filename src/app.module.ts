4e5364464e0e50e6898620b0c7027f9bbd4c79f7
lidordvir <lidor4001@gmail.com>
2024-02-26 15:30:37 +0200

    * Made AuthGuard global

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@ import { WorkoutModule } from './workout/workout.module';
 import { ExerciseModule } from './exercise/exercise.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModuleFactoryOptions } from '@nestjs/mongoose/dist/interfaces/mongoose-options.interface';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './common/guard/auth/auth.guard';
+import { JWTModule } from './common/jwt/jwt.module';
 
 @Module({
   imports: [
@@ -27,8 +30,15 @@ import { MongooseModuleFactoryOptions } from '@nestjs/mongoose/dist/interfaces/m
     UserModule,
     WorkoutModule,
     ExerciseModule,
+    JWTModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/common/guard/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JWTModule } from '../../jwt/jwt.module';
-
-@Module({
-  imports: [JWTModule],
-  controllers: [],
-  providers: [],
-})
-export class AuthModule {}
