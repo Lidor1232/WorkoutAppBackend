ddfbc936f71504b2a8f69e763db5c38b7c4ff403
lidordvir <lidor4001@gmail.com>
2024-02-26 10:28:19 +0200

    * Added to string service logs

--- src/utills/data-structure/string/string.service.ts
+++ src/utills/data-structure/string/string.service.ts
@@ -1,8 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { LoggerService } from '../../../common/logger/logger.service';
 
 @Injectable()
 export class StringService {
-  constructor() {}
+  constructor(private loggerService: LoggerService) {}
 
   stringsEqualOrThrow({
     string1,
@@ -11,8 +12,22 @@ export class StringService {
     string1: string;
     string2: string;
   }): void {
+    this.loggerService.logger.debug(
+      {
+        string1,
+        string2,
+      },
+      'Validating strings equal or throw',
+    );
     if (string1 !== string2) {
       throw new BadRequestException('Strings not equal');
     }
+    this.loggerService.logger.debug(
+      {
+        string1,
+        string2,
+      },
+      'Validated strings equal or throw',
+    );
   }
 }
