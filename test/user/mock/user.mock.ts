7a316ae19012de350358e077ba2b9b1301bfac2d
lidordvir <lidor4001@gmail.com>
2024-02-29 17:13:30 +0200

    * WIP

--- test/user/mock/user.mock.ts
+++ test/user/mock/user.mock.ts
@@ -1,7 +1,7 @@
 import { User } from '../../../src/user/user.schema';
 
 export const userMock: Omit<User, '_id'> = {
-  id: '65dcaf6412a35fb2fc93aaa9',
+  id: '65df0b176f17ecc8896d0df1',
   firstName: 'testFirstName',
   lastName: 'testLastName',
   userName: 'testUserName123',
